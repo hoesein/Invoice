7ebcfa0c99cb50cdecca191246da059e0e9df7cc
hoesein <a.heinsoe.a@gmail.com>
2020-03-25 16:10:41 +0630

    Updates

---
+++ precache-manifest.2783004ea1f07624389837681601f5cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a85333a40148752eee267e342ed18c4f",
+    "url": "/Invoice/index.html"
+  },
+  {
+    "revision": "67fd6fff7744e7804000",
+    "url": "/Invoice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b81e08c64091d5d8e698",
+    "url": "/Invoice/static/js/2.59ee6a5e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Invoice/static/js/2.59ee6a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67fd6fff7744e7804000",
+    "url": "/Invoice/static/js/main.6fb726da.chunk.js"
+  },
+  {
+    "revision": "67059d5de24b7711de0b",
+    "url": "/Invoice/static/js/runtime-main.ac9ab794.js"
+  },
+  {
+    "revision": "be9b17e3a6ebe8f8c0b3b3fc67d4a065",
+    "url": "/Invoice/static/media/cherry_charity.be9b17e3.jpg"
+  }
+]);
